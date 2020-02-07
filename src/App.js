da1b1973c432b9bce4e231d77f7b2d640e8fb609
Daniel Karski <daniel.karski@polidea.com>
2020-02-07 16:11:03 +0100

    Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Box = styled.div``
+
+function App() {
+  return (
+    <Box>Hello World!</Box>
+  );
+}
+
+export default App;
