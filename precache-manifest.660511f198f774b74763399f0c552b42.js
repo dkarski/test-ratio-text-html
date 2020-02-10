0ae669f582b8c928d3b34749de0f12ac99cfea4b
Daniel Karski <daniel.karski@polidea.com>
2020-02-10 15:39:04 +0100

    Updates

---
+++ precache-manifest.660511f198f774b74763399f0c552b42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2861789ddadd16d60ef3560573a6ce",
+    "url": "/test-ratio-text-html/index.html"
+  },
+  {
+    "revision": "5b675f7312d7ca9cef8a",
+    "url": "/test-ratio-text-html/static/js/2.a3dbc1eb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/test-ratio-text-html/static/js/2.a3dbc1eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c1ac0c7cab935e50dc9",
+    "url": "/test-ratio-text-html/static/js/main.9fba4b23.chunk.js"
+  },
+  {
+    "revision": "bf79dfe9715d154bffba",
+    "url": "/test-ratio-text-html/static/js/runtime-main.94562920.js"
+  }
+]);
